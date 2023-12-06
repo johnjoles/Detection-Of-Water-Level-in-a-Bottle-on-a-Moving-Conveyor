1b9553b866290df050fd7953af6c121d1bd8786a
Jesu John Joles Thadeus <131522981+johnjoles@users.noreply.github.com>
2023-12-06 15:05:54 +0100

    Update Master.py

--- Master.py
+++ Master.py
@@ -13,22 +13,17 @@ detector = Sl.Bottle(conf_threshold=0.75)
 Object_names = detector.Objname()
 
 
-# file = "detected_bottle111.jpg"
+
 def algorithm(image, b_w, b_y):
-    # dimension
     height = image.shape[0]
-    # print("Height of bottle:", height)
     width = image.shape[1]
-
+    
     # find the level of the water present:
-    # print(b_y)
-    # print(b_w)
-    Level = height - b_y
 
+    Level = height - b_y
     return Level
 
 
-# Bottle in ml:
 
 def ml_conversion(water_ml):
     water_ml = int((water_ml / 3))
@@ -42,8 +37,6 @@ def ml_conversion(water_ml):
 
 
 def level_detection(image):
-    # kernel = np.ones((7, 7), np.uint8)
-    # img_dilation = cv.dilate(image, kernel, iterations=1)
     grey_img = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     cv.imshow("GRAY", grey_img)
     blur_image = cv.GaussianBlur(image, (7, 7), 0)
@@ -51,7 +44,7 @@ def level_detection(image):
     cv.imshow("CANNY", canny)
 
     lines = cv.HoughLines(canny, 1, np.pi / 180, 30, np.array([]))
-    # print(len(lines))
+   
     x = 0
     y = 0
 
@@ -68,15 +61,12 @@ def level_detection(image):
         x2 = int(x0 - 1000 * (-b))
         y2 = int(y0 - 1000 * a)
 
-        # cv.line(image, (x1, y1), (x2, y2), (0, 0, 255), 3)
+       
         angle = np.arctan2(y2 - y1, x2 - x1) * 180. / np.pi
         if 0 < angle < 1:
             # print(angle)
             cv.line(image, (x1, y1), (x2, y2), (0, 0, 255), 3)
-            # print("x1:", x1)
-            # print("x2:", x2)
-            # print("y1:", y1)
-            # print("y2:", y2)
+           
             x = (x1 + x2) / 2
             y = (y1 + y2) / 2
 
@@ -95,18 +85,14 @@ def process(org_image):
     cy = 0
     for cnt in contour:
         area = cv.contourArea(cnt)
-        # print(area)
         perimeter = cv.arcLength(cnt, True)
-
         epsilon = 0.1 * perimeter
 
         approx = cv.approxPolyDP(cnt, epsilon, True)
         if 100 < area < 5000:
-            # print(area)
             M = cv.moments(cnt)
             cx = int(M['m10'] / M['m00'])
             cy = int(M['m01'] / M['m00'])
-            # print(cx, cy)
             cv.drawContours(org_image, [approx], -1, (0, 255, 255), 5)
     return cx, cy
 
@@ -122,22 +108,14 @@ def main(image):
         print("Level of Water is not visible to detect")
 
 
-# org_image = cv.imread("detected_bottle1.jpg")
-
-
 while True:
     done, image = vid.read()
     boundary = detector.ObjectDec(image, ObjectNames=Object_names)
-    # print(boundary)
     if len(boundary) == 4:
         print(boundary)
         x, y, w, h = boundary[0], boundary[1], boundary[2], boundary[3]
         Width = w
         Height = h
-        # cv.circle(image, (x, y), 2, (250, 250, 0), 1)
-        # cv.circle(image, (x + w, y), 2, (250, 0, 0), 1)
-        # cv.circle(image, (x, y + h), 2, (0, 250, 0), 1)
-        # cv.circle(image, (x + w, y + h), 2, (0, 0, 250), 1)
         cropped_one = detector.WarpImage(image, x, y, w, h, Width, Height)
         if len(boundary) > 0:
             cv.imshow("CropImage", cropped_one)
@@ -148,10 +126,6 @@ while True:
         print("no boundary no bottle detected")
 
     cv.imshow("Bottle", image)
-
-    # bottle = detector.getWarp(image,boundary)
-    if cv.waitKey(1) & 0xFF == ord('c'):
-        file = cv.imwrite(file, cropped_one)
 
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
